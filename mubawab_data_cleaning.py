f06b85ed41e936dad570515501c0651e80ff0fb9
Ilyas Oudghiri <84033760+iliasoudghiri@users.noreply.github.com>
2021-06-16 13:25:03 +0100

    Modified cleaning script and inherant csv file
    Added steps to cleaning such as
    - dealing with duplicates,
    - unnecessary spaces in the localisation variable,
    - imputed villas to have floor = 0

--- mubawab_data_cleaning.py
+++ mubawab_data_cleaning.py
@@ -7,15 +7,12 @@ Created on Wed Jun  9 13:33:12 2021
 """
 import pandas as pd 
 import numpy as np
-import re
+
 
 df = pd.read_csv('mubawab_listings.csv',dtype = object )
 
-
-
 #clean type column to keep only the first word
 df['Type'] = df['Type'].apply(lambda x: str(x).split(' ')[0])
-
 
 #clean localisation to get the neighbourhood
 df['Localisation'] = df['Localisation'].apply(lambda x: x.split('à')[0])
@@ -38,13 +35,13 @@ for i in range(len(df)):
         if "m²" in tags[t]:
             df.loc[i,'Area'] = tags[t]
             to_pop.append(t)
-        if "Pièces" in tags[t]:
+        if "Pièces" in tags[t] or "Pièce" in tags[t]:
             df.loc[i,'Rooms'] = tags[t]
             to_pop.append(t)
-        if "Chambres" in tags[t]:
+        if "Chambres" in tags[t] or "Chambre" in tags[t]:
             df.loc[i,'Bedrooms'] = tags[t]
             to_pop.append(t)
-        if "Salles de bains" in tags[t]:
+        if "Salles de bains" in tags[t] or "Salle de bain" in tags[t]:
             df.loc[i,'Bathrooms'] = tags[t]
             to_pop.append(t)
         if "étage" in tags[t]:
@@ -83,8 +80,21 @@ for idx in missing_type:
 #Drop listings with missing type for which title didn't contain type either
 df = df.dropna(subset = ['Type']).reset_index(drop = True)
 
+#Check and drop duplicates
+df.duplicated(subset = ['Type', 'Area', 'Price', 'Localisation']).sum()
+df = df.drop_duplicates(subset = ['Type', 'Area', 'Price', 'Localisation']).reset_index(drop= True)
+
+#Remove space after neighbourhoods
+df['Localisation'] = df.Localisation.str.strip()
+
+#Set value of floor to 0 for Villas, Riads and Houses
+df.loc[df[df['Type' ]!= 'Appartements'].index ,'Floor'] = 0
+
 #Drop title as we can't be sure about objectivity of informations
-df = df.drop(['Title'],axis=1).reset_index(drop = True)
+df = df.drop(['Title'],axis=1)
+
+#Add Price_m2 column
+df['Price_m2'] = df['Price']/df['Area']
 
 #Save df to csv file for further analysis and preprocessing
 df.to_csv('mubawab_listings_clean.csv',index=False)
